10b2ec1a7e9935f8defd4f3d469bdd9a5a9f784b
Ekene <ekeneezeifeomasam27@gmail.com>
2023-09-17 20:27:17 +0100

    form

--- src/components/Chatroom.tsx
+++ src/components/Chatroom.tsx
@@ -41,7 +41,11 @@ export default function Chatroom({roomId}:{roomId:string}) {
 
   console.log(session?.userId);
   
-  async function sendMessage() {
+  async function sendMessage(e:React.FormEvent<HTMLInputElement>) {
+    e.preventDefault()
+    if(!message) {
+      return
+    }
     const res = await axios.post('/api/messages', {
       text:message,
       roomId:roomId,
@@ -53,7 +57,7 @@ export default function Chatroom({roomId}:{roomId:string}) {
   }
 
   return (
-    <div className='flex flex-col px-[1rem] lg:px-[3.5rem]  pt-[5rem] w-[100vw] lg:w-[calc(100vw-20.25rem)] min-h-screen pb-[6.46rem] max-h-screen overflow-y-scroll'>
+    <form className='flex flex-col px-[1rem] lg:px-[3.5rem]  pt-[5rem] w-[100vw] lg:w-[calc(100vw-20.25rem)] min-h-screen pb-[6.46rem] max-h-screen overflow-y-scroll' onSubmit={sendMessage} >
       <div className='flex flex-col space-y-[2.37rem]'>
       {initialMessages?.map((msg) => {
         const date = parseDate(msg.createdAt) 
@@ -67,11 +71,11 @@ export default function Chatroom({roomId}:{roomId:string}) {
       </div>
       <div className='flex  h-[3.45rem] lg:h-[3.85rem] rounded-[.75rem] bg-[#3C393F] fixed mt-auto space-x-2 bottom-[1rem] lg:bottom-[2.46rem] lg:left-[23.46rem] left-[1rem] right-[1rem] lg:right-[3.46rem] items-center px-[.59rem] '>
         <input type="text" onChange={(e) => setMessage(e.target.value)} value={message} className='bg-transparent outline-none indent-5 w-full caret-white text-white' />
-        <Button onClick={sendMessage}  className='bg-[#2F80ED] flex justify-center items-center'>
+        <Button className='bg-[#2F80ED] flex justify-center items-center'>
         <SendHorizontal className='text-[#FFFFFF]' />
         
         </Button>
       </div>
-    </div>
+    </form>
   )
 }
